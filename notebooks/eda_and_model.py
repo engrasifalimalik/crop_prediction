32c43a6486e37bf7437ee0d6d93944401716b388
engrasifalimalik <135517382+engrasifalimalik@users.noreply.github.com>
2026-03-26 10:25:36 +0500

    Update eda_and_model.py

--- notebooks/eda_and_model.py
+++ notebooks/eda_and_model.py
@@ -1,1 +1,17 @@
+# Import libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Load dataset
+df = pd.read_csv("../data/crop_data.csv")
+
+# Show data
+print(df.head())
+
+# Basic info
+print(df.info())
+
+# Correlation heatmap
+sns.heatmap(df.corr(), annot=True)
+plt.show()
